6099d98c8b4659868b2f48c68d9b2f072e51b309
dennycox <denny.cox@chello.nl>
2020-04-22 18:36:46 +0200

    Added a testmethod

--- CircusTrain.Test/CircusTrainTest.cs
+++ CircusTrain.Test/CircusTrainTest.cs
@@ -8,22 +8,43 @@ namespace CircusTrain.Tests
     public class CircusTrainTest
     {
         [TestMethod]
-        public void AddAnimals_Add3Animals_3AnimalsAdded()
+        public void AddAnimals_Add2Animals_True()
         {
-            // 1. Arrange
+            // Arrange
             var train = new CircusTrain();
             var animals = new List<Animal>()
             {
-                new Animal{ Diet=Diet.Carnivore, Size=Size.Large },
-                new Animal{ Diet=Diet.Carnivore, Size=Size.Large },
-                new Animal{ Diet=Diet.Carnivore, Size=Size.Large },
+                new Animal(Diet.Carnivore, Size.Large),
+                new Animal(Diet.Carnivore, Size.Large),
             };
 
-            // 2. Act
+            // Act
             train.AddAnimals(animals);
 
-            // 3. Assert
-            Assert.AreEqual(3, train.WagonAmount);
+            // Assert
+            Assert.AreEqual(2, train.GetWagons().Count);
+        }
+
+        [TestMethod]
+        public void AddAnimals_Add3Animals_True()
+        {
+            // Arrange
+            var train = new CircusTrain();
+            var animals = new List<Animal>()
+            {
+                new Animal(Diet.Carnivore, Size.Large),
+                new Animal(Diet.Herbivore, Size.Medium),
+                new Animal(Diet.Carnivore, Size.Medium),
+            };
+
+            // Act
+            train.AddAnimals(animals);
+            IReadOnlyList<Animal> animalsInTrain = train.GetAnimals();
+
+            // Assert
+            Assert.IsTrue(animalsInTrain[0].Size == Size.Large & animalsInTrain[0].Diet == Diet.Carnivore);
+            Assert.IsTrue(animalsInTrain[1].Size == Size.Medium & animalsInTrain[1].Diet == Diet.Carnivore);
+            Assert.IsTrue(animalsInTrain[2].Size == Size.Medium & animalsInTrain[2].Diet == Diet.Herbivore);
         }
     }
 }
